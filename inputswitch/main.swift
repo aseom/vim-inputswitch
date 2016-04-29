0510dd48fa06979139c342cbc6273b0a97a93b17
aseom <hm9599@gmail.com>
2016-04-29 09:44:53 +0900

    기존 것 복사

---
+++ inputswitch/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  inputswitch
+//
+//  ASeom Han <hm9599@gmail.com>
+//
+
+import Foundation
+import Carbon
+
+func getInputSourceLanguage(inputSource: TISInputSource) -> String {
+    let ptr = TISGetInputSourceProperty(inputSource, kTISPropertyInputSourceLanguages)
+    let cfLangs: CFArray = Unmanaged.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
+    
+    return (cfLangs as Array)[0] as! String
+}
+
+func getInputSourceName(inputSource: TISInputSource) -> String {
+    let ptr = TISGetInputSourceProperty(inputSource, kTISPropertyLocalizedName)
+    let cfName: CFString = Unmanaged.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
+    
+    return cfName as String
+}
+
+func setInputSource(language: String) {
+    let currentInputSource = TISCopyCurrentKeyboardInputSource().takeRetainedValue()
+    if language != getInputSourceLanguage(currentInputSource) {
+        
+        let inputSource = TISCopyInputSourceForLanguage(language).takeRetainedValue()
+        TISSelectInputSource(inputSource)
+        
+        print("Input Source Changed: \"" + getInputSourceName(inputSource) + "\"")
+    }
+}
+
+if(Process.arguments.count >= 2) {
+    setInputSource(Process.arguments[1])
+} else {
+    print("Missing argument: language code")
+    exit(1)
+}
+
