b9467b294ec6df1e33e12b00f2358615d8e18013
aseom <hm9599@gmail.com>
2016-04-30 23:58:39 +0900

    불필요한 import 삭제

--- inputswitch/main.swift
+++ inputswitch/main.swift
@@ -5,30 +5,29 @@
 //  ASeom Han <hm9599@gmail.com>
 //
 
-import Foundation
 import Carbon
 
 func getInputSourceLanguage(inputSource: TISInputSource) -> String {
     let ptr = TISGetInputSourceProperty(inputSource, kTISPropertyInputSourceLanguages)
     let cfLangs: CFArray = Unmanaged.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
-    
+
     return (cfLangs as Array)[0] as! String
 }
 
 func getInputSourceName(inputSource: TISInputSource) -> String {
     let ptr = TISGetInputSourceProperty(inputSource, kTISPropertyLocalizedName)
     let cfName: CFString = Unmanaged.fromOpaque(COpaquePointer(ptr)).takeUnretainedValue()
-    
+
     return cfName as String
 }
 
 func setInputSource(language: String) {
     let currentInputSource = TISCopyCurrentKeyboardInputSource().takeRetainedValue()
     if language != getInputSourceLanguage(currentInputSource) {
-        
+
         let inputSource = TISCopyInputSourceForLanguage(language).takeRetainedValue()
         TISSelectInputSource(inputSource)
-        
+
         // Print without newline
         print("Input Source Changed: \"" + getInputSourceName(inputSource) + "\"", terminator:"")
     }
@@ -40,4 +39,3 @@ if(Process.arguments.count >= 2) {
     print("Missing argument: language code")
     exit(1)
 }
-
